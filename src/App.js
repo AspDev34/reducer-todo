758d402db5da4ad8adc122b3b55fbeacbe4c1ab3
Ike Otu <ikeotu@msn.com>
2020-02-17 17:15:42 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO WORLD!!! I'M LEARNING TO CODE!!! MONEY MONEY MONEY!!!!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const TodoForm = (props) => {
+
+    const [newTodo, setNewTodo] = useState('');
+
+    const handleChange = event => {
+        setNewTodo(event.target.value)
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        props.makeTodo(newTodo);
+        setNewTodo('');
+    };
+
+    return (
+
+        <form onSubmit={handleSubmit}>
+
+            <input 
+            type="text"
+            value={newTodo}
+            name="newTodo"
+            onChange={handleChange}
+            />
+
+            <button>Add Todo item</button>
+            <button onClick={()=>props.clearComplete()}>Clear/Completed</button>
+        </form>
+    );
+};
+
+export default TodoForm;
